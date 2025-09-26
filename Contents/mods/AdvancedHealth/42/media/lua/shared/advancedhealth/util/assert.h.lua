c1c0e5f35d51eeb5f6fab3628981a023ad04451f
JabDoesThings <JabDoesThings@github.com>
2025-09-26 13:00:36 -0500

    Upgraded assert.

--- Contents/mods/AdvancedHealth/42/media/lua/shared/advancedhealth/util/assert.h.lua
+++ Contents/mods/AdvancedHealth/42/media/lua/shared/advancedhealth/util/assert.h.lua
@@ -1,53 +1,105 @@
 --- @meta
 
+--- @author JabDoesThings, asledgehammer 2025
+
 --- @alias AuditType 'nil' | 'number' | 'string' | 'boolean' | 'table' | 'function' | 'thread' | 'userdata'
 
 --- @class AssertAPI
 local API = {};
 
---- @param path string
---- @param value any
-function API.assertNonNil(path, value) end
+--- @param path string The name of (or path to) the variable.
+--- @param value any The value of the variable.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertNonNil(path, value, depth) end
 
---- @param path string
---- @param value any
+--- @param path string The name of (or path to) the variable.
+--- @param value any The value of the variable.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertNil(path, value, depth) end
+
+--- @param path string The name of (or path to) the variable.
+--- @param value any The value of the variable.
 --- @param type AuditType
-function API.assertType(path, value, type) end
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertType(path, value, type, depth) end
 
---- @param path string
---- @param value any
-function API.assertString(path, value) end
+--- @param path string The name of (or path to) the variable.
+--- @param value string The value of the variable.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertString(path, value, depth) end
 
---- @param path string
---- @param value any
+--- @param path string The name of (or path to) the variable.
+--- @param value string The value of the variable.
 --- @param expectedValue string
-function API.assertStringExact(path, value, expectedValue) end
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertStringExact(path, value, expectedValue, depth) end
 
---- @param path string
---- @param value any
-function API.assertNonEmptyString(path, value) end
+--- @param path string The name of (or path to) the variable.
+--- @param value string The value of the variable.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertNonEmptyString(path, value, depth) end
 
---- @param path string
---- @param value any
-function API.assertNumber(path, value) end
+--- @param path string The name of (or path to) the variable.
+--- @param value number THe value of the variable.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertNumber(path, value, depth) end
 
---- @param path string
---- @param value any
+--- @param path string The name of (or path to) the variable.
+--- @param value number The value of the variable.
 --- @param expectedValue number
-function API.assertNumberExact(path, value, expectedValue) end
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertNumberExact(path, value, expectedValue, depth) end
 
---- @param path string
---- @param value any
-function API.assertUnsignedNumber(path, value) end
+--- @param path string The name of (or path to) the variable.
+--- @param value number The value of the variable.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertUnsignedNumber(path, value, depth) end
 
---- @param path string
---- @param value any
-function API.assertPositiveNumber(path, value) end
+--- @param path string The name of (or path to) the variable.
+--- @param value number The value of the variable.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertPositiveNumber(path, value, depth) end
 
---- @param path string
---- @param value any
-function API.assertNegativeNumber(path, value) end
+--- @param path string The name of (or path to) the variable.
+--- @param value number The value of the variable.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertNegativeNumber(path, value, depth) end
 
---- @param path string
---- @param value any
-function API.assertNumberRange(path, value, min, max) end
+--- @param path string The name of (or path to) the variable.
+--- @param value number The value of the variable.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertNumberRange(path, value, min, max, depth) end
+
+--- @param path string The name of (or path to) the variable.
+--- @param value string The value of the variable.
+--- @param values string[] All enum values.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertEnum(path, value, values, depth) end
+
+--- @param path string The name of (or path to) the variable.
+--- @param value string | nil The value of the variable.
+--- @param values string[] All enum values.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertEnumOrNil(path, value, values, depth) end
+
+--- @param path string The name of (or path to) the variable.
+--- @param value table The value of the variable.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertTable(path, value, depth) end
+
+--- @param path string The name of (or path to) the variable.
+--- @param value any[] The value of the variable.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertArray(path, value, depth) end
+
+--- @param path string The name of (or path to) the variable.
+--- @param value any[] The value of the variable.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertNonEmptyArray(path, value, depth) end
+
+--- Asserts that both the array AND each entry are not empty.
+--- 
+--- @param path string The name of (or path to) the variable.
+--- @param value string[] The value of the variable.
+--- @param depth integer|nil Tracks the depth of calls to point towards the base assert check call.
+function API.assertNonEmptyStringArray(path, value, depth) end
--- Contents/mods/AdvancedHealth/42/media/lua/shared/advancedhealth/util/assert.lua
+++ Contents/mods/AdvancedHealth/42/media/lua/shared/advancedhealth/util/assert.lua
@@ -1,19 +1,70 @@
+--- @author JabDoesThings, asledgehammer 2025
+
 local API = {};
 
 --- @cast API AssertAPI
 
-function API.assertNonNil(path, value)
+--- @param t any[]
+--- 
+--- @return boolean
+local function isArray(t)
+    local i = 0;
+    for _ in pairs(t) do
+        i = i + 1;
+        if t[i] == nil then return false end
+    end
+    return true;
+end
+
+--- @param array string[]
+--- @param pretty boolean
+---
+--- @return string
+local function enumsToString(array, pretty)
+    if #array == 0 then return '[]' end
+    local s = '';
+    if pretty then
+        for i = 1, #array do
+            if s == '' then
+                s = array[i];
+            else
+                s = s .. ',\n\t' .. array[i];
+            end
+        end
+        return string.format('[\n\t%s\n]', s);
+    else
+        for i = 1, #array do
+            if s == '' then
+                s = array[i];
+            else
+                s = s .. ', ' .. array[i];
+            end
+        end
+        return string.format('[%s]', s);
+    end
+end
+
+function API.assertNil(path, value, depth)
+    depth = depth or 2;
+    if value ~= nil then
+        error(string.format('The value \'%s\' is NOT nil.', path), 2);
+    end
+end
+
+function API.assertNonNil(path, value, depth)
+    depth = depth or 2;
     if value == nil then
         error(string.format('The value \'%s\' is nil.', path), 2);
     end
 end
 
-function API.assertType(path, value, t)
+function API.assertType(path, value, t, depth)
+    depth = depth or 2;
     API.assertNonNil(path, value);
     local tvalue = type(value);
     if tvalue ~= t then
         error(
-            string.format('The value \'%s\' is not a %s. { type = %s, value = %s }',
+            string.format('The value \'%s\' is NOT a %s. { type = %s, value = %s }',
                 path, t,
                 tvalue, tostring(value)
             ),
@@ -22,41 +73,46 @@ function API.assertType(path, value, t)
     end
 end
 
-function API.assertString(path, value)
+function API.assertString(path, value, depth)
+    depth = depth or 2;
     API.assertNonNil(path, value);
     API.assertType(path, value, 'string');
 end
 
-function API.assertStringExact(path, value, expectedValue)
+function API.assertStringExact(path, value, expectedValue, depth)
+    depth = depth or 2;
     API.assertString(path, value);
     if value ~= expectedValue then
         error(
-            string.format('The value \'%s\' isn\'t the value \'%s\'! { type = %s, value = %s }',
+            string.format('The value \'%s\' is NOT the value \'%s\'! { type = %s, value = %s }',
                 path, expectedValue,
                 type(value), tostring(value)
             ),
-            2
+            depth
         );
     end
 end
 
-function API.assertNonEmptyString(path, value)
-    API.assertString(path, value);
+function API.assertNonEmptyString(path, value, depth)
+    depth = depth or 2;
+    API.assertString(path, value, depth + 1);
     if value == '' then
         error(string.format('The value \'%s\' is an empty string.'), 2);
     end
 end
 
-function API.assertNumber(path, value)
-    API.assertNonNil(path, value);
-    API.assertType(path, value, 'number');
+function API.assertNumber(path, value, depth)
+    depth = depth or 2;
+    API.assertNonNil(path, value, depth + 1);
+    API.assertType(path, value, 'number', depth + 1);
 end
 
-function API.assertNumberExact(path, value, expectedValue)
-    API.assertNumber(path, value);
+function API.assertNumberExact(path, value, expectedValue, depth)
+    depth = depth or 2;
+    API.assertNumber(path, value, depth + 1);
     if value ~= expectedValue then
         error(
-            string.format('The value \'%s\' isn\'t the value %s! { type = %s, value = %s }',
+            string.format('The value \'%s\' is NOT the value %s! { type = %s, value = %s }',
                 path, expectedValue,
                 type(value), tostring(value)
             ),
@@ -65,49 +121,53 @@ function API.assertNumberExact(path, value, expectedValue)
     end
 end
 
-function API.assertUnsignedNumber(path, value)
-    API.assertNumber(path, value);
+function API.assertUnsignedNumber(path, value, depth)
+    depth = depth or 2;
+    API.assertNumber(path, value, depth + 1);
     if value < 0 then
         error(
-            string.format('The value \'%s\' is not an unsigned value. { value = %s }',
+            string.format('The value \'%s\' is NOT an unsigned value. { value = %s }',
                 path,
                 tostring(value)
             ),
-            2
+            depth
         );
     end
 end
 
-function API.assertPositiveNumber(path, value)
-    API.assertNumber(path, value);
+function API.assertPositiveNumber(path, value, depth)
+    depth = depth or 2;
+    API.assertNumber(path, value, depth + 1);
     if value < 1 then
         error(
-            string.format('The value \'%s\' is not a positive value. { value = %s }',
+            string.format('The value \'%s\' is NOT a positive value. { value = %s }',
                 path,
                 tostring(value)
             ),
-            2
+            depth
         );
     end
 end
 
-function API.assertNegativeNumber(path, value)
-    API.assertNumber(path, value);
+function API.assertNegativeNumber(path, value, depth)
+    depth = depth or 2;
+    API.assertNumber(path, value, depth + 1);
     if value > -1 then
         error(
-            string.format('The value \'%s\' is not a negative value. { value = %s }',
+            string.format('The value \'%s\' is NOT a negative value. { value = %s }',
                 path,
                 tostring(value)
             ),
-            2
+            depth
         );
     end
 end
 
-function API.assertNumberRange(path, value, min, max)
-    API.assertNumber('min', min);
-    API.assertNumber('max', max);
-    API.assertNumber(path, value);
+function API.assertNumberRange(path, value, min, max, depth)
+    depth = depth or 2;
+    API.assertNumber('min', min, depth + 1);
+    API.assertNumber('max', max, depth + 1);
+    API.assertNumber(path, value, depth + 1);
     if value < min or value > max then
         error(
             string.format('The value \'%s\' is outside the value range. { min = %.4f, max = %.4f, value = %.4f }',
@@ -116,9 +176,76 @@ function API.assertNumberRange(path, value, min, max)
                 tostring(max),
                 tostring(value)
             ),
-            2
+            depth
         );
     end
+end
+
+function API.assertTable(path, value, depth)
+    depth = depth or 2;
+    API.assertNonNil(path, value, depth + 1);
+    API.assertType(path, value, 'table', depth + 1);
+end
+
+function API.assertArray(path, value, depth)
+    depth = depth or 2;
+    API.assertNonNil(path, value, depth + 1);
+
+    if not isArray(value) then
+        error(
+            string.format(
+                'The value \'%s\' is NOT an array. { type = %s, value = %s }',
+                path,
+                type(value),
+                tostring(value)
+            ),
+            depth
+        );
+    end
+end
+
+function API.assertNonEmptyArray(path, value, depth)
+    depth = depth or 2;
+    API.assertArray(path, value, depth + 1);
+    if #value == 0 then
+        error(string.format('The value \'%s\' is an empty array.', path), 2);
+    end
+end
+
+function API.assertNonEmptyStringArray(path, value, depth)
+    depth = depth or 2;
+    API.assertNonEmptyArray(path, value, depth + 1);
+    for i = 1, #value do
+        API.assertNonEmptyString(string.format('%s[%i]', path, i), value[i], depth + 1);
+    end
+end
+
+function API.assertEnum(path, value, values, depth)
+    depth = depth or 2;
+    API.assertNonEmptyString(path, value, depth + 1);
+    API.assertNonEmptyStringArray('types', values, depth + 1);
+
+    for i = 1, #values do
+        if value == values[i] then return end
+    end
+
+    local sTypes = enumsToString(values, false);
+    error(
+        string.format(
+            'The value \'%s\' is NOT one of the following enums: %s { value = %s }'
+            ,
+            path,
+            sTypes,
+            tostring(value)
+        ),
+        depth
+    );
+end
+
+function API.assertEnumOrNil(path, value, values, depth)
+    depth = depth or 2;
+    if value == nil then return end
+    API.assertEnum(path, value, values, depth + 1);
 end
 
 return API;
